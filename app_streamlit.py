032d846029b050e687596b3e5c312f798c8dc75c
Pranav kumar <115449562+KumarPranavv@users.noreply.github.com>
2025-01-06 01:44:49 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -19,7 +19,7 @@ st.set_page_config(
 # -----------------------------------------------------------------------------
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("Images/LandLogo.png", width=480)
+    st.image("Images/LandLogo.png", width=400)
 
 # -----------------------------------------------------------------------------
 # Title
